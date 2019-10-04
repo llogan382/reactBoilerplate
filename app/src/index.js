85d31b7b68b9549b92ed1007d546fbdc0bea1850
Luke Logan <llogan382@gmail.com>
2019-10-04 16:55:08 -0400

    adds reusable static components

--- app/src/index.js
+++ app/src/index.js
@@ -2,31 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './app.css';
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header>
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
+            <h1>{props.title}</h1>
+            <span className="stats">Players: {props.totalPlayers}</span>
         </header>
     );
 }
-const Player = () => {
+const Player = (props) => {
     return (
         <div className="player">
             <span className="player-name">
-                Luke
-        </span>
-            <Counter />
+                {props.playerName}
+            </span>
+            <Counter playerScore={props.playerScore} />
         </div>
-
     );
 }
 
-const Counter = () => {
+const Counter = (props) => {
     return (
         <div className="counter">
             <button className="counter-action decrement"> - </button>
-            <span className="counter-score">35</span>
+            <span className="counter-score">{props.playerScore}</span>
             <button className="counter-action increment"> + </button>
         </div>
     );
@@ -35,9 +34,12 @@ const Counter = () => {
 const App = () => {
     return (
         <div className="scoreboard">
-            <Header />
+            <Header title="Scoreboard" totalPlayers={4} />
             { /*  Players list!*/}
-            <Player />
+            <Player playerName="Pooks" playerScore={100} />
+            <Player playerName="Sleepytime" playerScore={20} />
+            <Player playerName="Cleo" playerScore={54} />
+            <Player playerName="Cheese" playerScore={630} />
         </div>
     )
 }
